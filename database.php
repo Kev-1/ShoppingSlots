93034282d910a333dd7d8ef20b6743ca24c8d85a
Kev-1 <xolto@outlook.com>
2020-05-26 16:20:01 +0700

    initial commit

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php // connect.php allows connection to the database
+    $hn='127.0.0.1';
+	$db = 'idn';
+	$un = 'idn';
+	$pw = 'coronasucksdick';
+	
+	$conn = new mysqli($hn,$un,$pw,$db);
+	
+	if ($conn->connect_error)
+		{ die($conn->connect_error);
+		echo 'Unable to connect to MySQL, contact System Administrator.';
+	}
+	else {
+
+    };
+?>
+  
