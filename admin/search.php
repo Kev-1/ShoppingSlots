eab4bbc780304050f27c754c39aa2962c8c0e2db
Kev-1 <xolto@outlook.com>
2020-06-09 15:46:50 +0700

    properly do search algorithm

--- admin/search.php
+++ admin/search.php
@@ -10,11 +10,11 @@
 	<body class="is-preload">
 		<div id="page-wrapper">
 			<header id="header">
-				<h1><a href="index.html">Grandlucky</a></h1>
+				<h1><a href="index.html">Grandlucky ADMIN</a></h1>
 					<nav id="nav">
 						<ul>
 						<li class="special">
-							<a href="#menu" class="menuToggle"><span>Menu</span></a>
+							<a href="#menu" class="menuToggle"><span>Admin Menu</span></a>
 							<div id="menu">
 								<ul>
 									<li><a href="index.html">Home</a></li>
@@ -34,27 +34,106 @@
 									if(!isset($_SESSION['user'])) {
 										header("Location: login.php");
 									} else {
-									$query  = "SELECT * FROM slots INNER JOIN locations on slots.location=locations.id where date=$date AND time=$time AND location=$location";
-									$result = $conn->query($query);
-									if (!$result) die ("Database access failed: " . $conn->error);
-									$rows = $result->num_rows;
+									
+									
+									if (isset($_POST['location']) == false) {
+										$locationQuery  = "SELECT * FROM locations";
+										$locationResult = $conn->query($locationQuery);
+										if (!$locationResult) die ("Database access failed: " . $conn->error);
+										$locationRows = $locationResult->num_rows;
+										
+										echo <<<_END
+										<form action="search.php" method="post">
+										<div class="row gtr-uniform">
+											<div class="col-12">
+												<h3>Select a store:</h3>
+												<select name="location" id="location" required>
+													<option value="">Select a Location</option>
+_END;
+										
+										for ($i = 0 ; $i < $locationRows ; ++$i) {
+											$locationResult->data_seek($i);
+											$locationRow = $locationResult->fetch_array(MYSQLI_ASSOC);
+											echo "<option value=".$locationRow['location_id'].">".$locationRow['location_name']."</option>";
+										};
+										
+										echo <<<_END
+												</select>
+											</div>
+_END;
+										
 
-									echo <<<_END
-									<div class="col-12">
-										<h2>Staff panel</h2>
-										<a href="search.php" class="button">Search Specific Data</a>
-										<div class="table-wrapper">
-											<table>
-												<thead>
-													<tr>
-														<th>Name</th>
-														<th>Phone Number</th>
-														<th>Date</th>
-														<th>Time</th>
-														<th>Code</th>
-														<th>Location Name</th>
-													</tr>
-												</thead>
+											$today = strtotime("today");
+											$today2 = date("Y-m-d", $today);
+											$todayText = date("d-m-y", $today);
+											$tomorrow = strtotime("tomorrow");
+											$tomorrow2 = date("Y-m-d", $tomorrow);
+											$tomorrowText = date("d-m-y", $tomorrow);
+											$twodays = strtotime("+2 Days");
+											$twodays2 = date("d-m-y", $twodays);
+											$twodaysText = date("d-m-y", $twodays);
+											
+											echo <<<_END
+											<div class="col-12">
+												<h3>Select a date:</h3>
+												<select name="date" id="date" required>
+													<option value="">Select a Date</option>
+													<option value="$today2">$todayText</option>
+													<option value="$tomorrow2">$tomorrowText</option>
+													<option value="$twodays2">$twodaysText</option>
+												</select>
+											</div>
+											<div class="col-12">
+												<h3>Select a time:</h3>
+												<select name="time" id="time" required>
+												
+											
+_END;
+										$time2 = strtotime("Today 09:00am");
+										$time = date("h:i:s", $time2);
+										
+										for($k = 0; $k < 12; ++$k) {
+											echo '<option value='.$time.'>'.$time.'</option>';
+											$time->modify('+1 hour');
+										}
+										
+										echo <<<_END
+											</div>
+											<div class="col-12">
+												<input type="submit">
+											</div>
+										</div>
+_END;
+									};
+										
+									if(isset($_POST['location']) && isset($_POST['date']) && isset($_POST['time'])) {
+									
+										$time = $_POST['time'];
+										$date = $_POST['date'];
+										$location = $_POST['location'];
+										
+										$query  = "SELECT * FROM slots INNER JOIN locations on slots.location=locations.id where date=$date AND time=$time AND location=$location";
+										$result = $conn->query($query);
+										if (!$result) die ("Database access failed: " . $conn->error);
+										$rows = $result->num_rows;
+										
+									
+										echo <<<_END
+										<div class="col-12">
+											<h2>Staff panel</h2>
+											<a href="search.php" class="button">Search Specific Data</a>
+											<div class="table-wrapper">
+												<table>
+													<thead>
+														<tr>
+															<th>Name</th>
+															<th>Phone Number</th>
+															<th>Date</th>
+															<th>Time</th>
+															<th>Code</th>
+															<th>Location Name</th>
+														</tr>
+													</thead>
 _END;
 
 										$rows = $result->num_rows;
@@ -70,12 +149,13 @@ _END;
 											echo '<td>'.$row['code'].'</td>';
 											echo '<td>'.$row['location_name'].'</td>';
 											echo '</tr>';
-									}
-								echo <<<_END
-								</table>
-								</div>
-								</div>
+										}
+										echo <<<_END
+											</table>
+										</div>
+									</div>
 _END;
+									}
 								};
 								?>
 							</div>
