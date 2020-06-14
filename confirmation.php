25991d00ece148dbe942dcdc53ceb17ae1e5612c
Kev-1 <xolto@outlook.com>
2020-06-15 01:50:29 +0700

    an attempt on validation of confirmation

--- confirmation.php
+++ confirmation.php
@@ -81,86 +81,99 @@
 										
 									}
 									
-									$code = rand(100000000, 999999999); //random generated code.
-									
-									//while loop to check if checkExists will return one.
-									$b = true;
-									while($b == true) {
-										$findQuery = "select * from slots where code=\"$code\"";
-										$findResult = $conn->query($findQuery);
-										if (!$findResult) die ("Database access failed: " . $conn->error);
-										if($findResult->num_rows > 0) {
-											$code = rand(100000000, 999999999);
-										} else {
-											$b = false;
-										}
-									}
-									
-									//retrieve location name again.
-									$locationQuery  = "SELECT * FROM locations where location_id=$location";
-									$locationResult = $conn->query($locationQuery);
-									if (!$locationResult) die ("Database access failed: " . $conn->error);
-									$locationRows = $locationResult->num_rows;
-									for ($i = 0 ; $i < $locationRows ; ++$i) {
-										$locationResult->data_seek($i);
-										$locationRow = $locationResult->fetch_array(MYSQLI_ASSOC);
-										$location_name = $locationRow['location_name'];
-									}
-									
-									//INSERT TO DATABASE
-									$insertQuery = "INSERT INTO slots (name, phone, location, date, time, code) VALUES (\"$name\", \"$phone\", \"$location\", \"$date\", \"$time\", \"$code\")";
-									$insertResult = $conn->query($insertQuery);
-									if (!$insertResult) echo ("INSERT failed: " . $conn->error . "Report to administrator.");
+									if(validationcheck()) {
+										$code = rand(100000000, 999999999); //random generated code.
 
-									echo <<<_END
-											<div class="table-wrapper">
-												<h3 align="center">Your unique code:</h3>
-												<h2 align="center">$code</h2>
-												<h3 align="center">Details of person: </h3>
-													<table>
-														<tbody>
-															<tr>
-																<td>Name:</td>
-																<td>$name</td>
-															</tr>
-															<tr>
-																<td>Phone:</td>
-																<td>$phone</td>
-															</tr>
-															<tr>
-																<td>Email:</td>
-																<td>$email</td>
-															</tr>
-														</tbody>
-													</table>
-												<h3 align="center">Details of Booking: </h3>
-													<table>
-														<tbody>
-															<tr>
-																<td>Date:</td>
-																<td>$date</td>
-															</tr>
-															<tr>
-																<td>Time:</td>
-																<td>$time</td>
-															</tr>
-															<tr>
-																<td>Location:</td>
-																<td>$location_name</td>
-															</tr>
-														</tbody>
-													</table>
-												<h4 align="center">Please finish your shopping by one hour in order to reduce the number of people in the store. Thank your for your co-operation</h4>
-											</div>
+										//while loop to check if checkExists will return one.
+										$b = true;
+										while($b == true) {
+											$findQuery = "select * from slots where code=\"$code\"";
+											$findResult = $conn->query($findQuery);
+											if (!$findResult) die ("Database access failed: " . $conn->error);
+											if($findResult->num_rows > 0) {
+												$code = rand(100000000, 999999999);
+											} else {
+												$b = false;
+											}
+										}
+
+										//retrieve location name again.
+										$locationQuery  = "SELECT * FROM locations where location_id=$location";
+										$locationResult = $conn->query($locationQuery);
+										if (!$locationResult) die ("Database access failed: " . $conn->error);
+										$locationRows = $locationResult->num_rows;
+										for ($i = 0 ; $i < $locationRows ; ++$i) {
+											$locationResult->data_seek($i);
+											$locationRow = $locationResult->fetch_array(MYSQLI_ASSOC);
+											$location_name = $locationRow['location_name'];
+										}
+
+										//INSERT TO DATABASE
+										$insertQuery = "INSERT INTO slots (name, phone, location, date, time, code) VALUES (\"$name\", \"$phone\", \"$location\", \"$date\", \"$time\", \"$code\")";
+
+										//check email inserted.
+										$insertResult = $conn->query($insertQuery);
+										if (!$insertResult) echo ("INSERT failed: " . $conn->error . "Report to administrator.");
+
+										echo <<<_END
+												<div class="table-wrapper">
+													<h3 align="center">Your unique code:</h3>
+													<h2 align="center">$code</h2>
+													<h3 align="center">Details of person: </h3>
+														<table>
+															<tbody>
+																<tr>
+																	<td>Name:</td>
+																	<td>$name</td>
+																</tr>
+																<tr>
+																	<td>Phone:</td>
+																	<td>$phone</td>
+																</tr>
+																<tr>
+																	<td>Email:</td>
+																	<td>$email</td>
+																</tr>
+															</tbody>
+														</table>
+													<h3 align="center">Details of Booking: </h3>
+														<table>
+															<tbody>
+																<tr>
+																	<td>Date:</td>
+																	<td>$date</td>
+																</tr>
+																<tr>
+																	<td>Time:</td>
+																	<td>$time</td>
+																</tr>
+																<tr>
+																	<td>Location:</td>
+																	<td>$location_name</td>
+																</tr>
+															</tbody>
+														</table>
+													<h4 align="center">Please finish your shopping by one hour in order to reduce the number of people in the store. Thank your for your co-operation</h4>
+												</div>
 _END;
+									} else {
+										echo <<<_END
+									<div class="col-12">
+										<h3>Error: $msg</h3>
+									</div>
+									<div class="col-12">
+										<a href="./index.html" class="button">Go back</a>
+									</div>
+_END;
+									}
 								} else {
 									echo <<<_END
-										<div class="col-12">
-											<h3>Error: No valid input was recieved. Click the button below to go back to the main page.</h3>		
-										</div>
-										<div class="col-12">
-											<a href="./index.html" class="button">Go back</a>
-										</div>
+									<div class="col-12">
+										<h3>Error: No valid input was recieved. Click the button below to go back to the main page.</h3>		
+									</div>
+									<div class="col-12">
+										<a href="./index.html" class="button">Go back</a>
+									</div>
 _END;
 								}
 								$conn->close();
